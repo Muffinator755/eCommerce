517fdc4c4993fb13ae2b01566e3cf0d2ef5fce83
Muffinator755 <40366122+Muffinator755@users.noreply.github.com>
2018-07-31 08:34:13 -0600

    Create test.js

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class test extends Component {
+    render() {
+        return (
+            <div>
+                The Power I hold
+            </div>
+        );
+    }
+}
