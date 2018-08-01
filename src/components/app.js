0f9240c5d389da6ba1564a95a2f22144a423923f
Code <Code@batc.local>
2018-08-01 08:05:31 -0600

    Added a simple header

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>eCommerce</h1>
+        <Header/>
       </div>
     );
   }
